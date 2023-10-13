1df231be3ff73c537b8bcc2a29325ea2da7540f7
VouDoo <maxgrymonprez@live.fr>
2023-10-13 13:27:47 +0200

    Fix settings.py

--- bisellium/settings.py
+++ bisellium/settings.py
@@ -4,6 +4,6 @@ env = Env()
 env.read_env()  # read .env file, if it exists
 
 PROMETHEUS_MULTIPROC_DIR = env.str("PROMETHEUS_MULTIPROC_DIR", default="./.metrics")
-LUDUS_ENDPOINT = env.str("LUDUS_ENDPOINT ", default="http://localhost:8081")
-ARENA_ENDPOINT = env.str("ARENA_ENDPOINT ", default="http://localhost:8082")
-PODIUM_ENDPOINT = env.str("PODIUM_ENDPOINT ", default="http://localhost:8083")
+LUDUS_ENDPOINT = env.str("LUDUS_ENDPOINT", default="http://localhost:8081")
+ARENA_ENDPOINT = env.str("ARENA_ENDPOINT", default="http://localhost:8082")
+PODIUM_ENDPOINT = env.str("PODIUM_ENDPOINT", default="http://localhost:8083")
